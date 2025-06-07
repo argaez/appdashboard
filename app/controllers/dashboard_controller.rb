64b386d6260aae736e3b684d2f787f18d4af27d9
argaez <mojiargaez@gmail.com>
2025-06-07 11:56:09 -0500

    Creación del controlador Dashboard, vista inicial, ruta principal

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,13 @@
+class DashboardController < ApplicationController
+  def index
+
+  end
+  
+  def show
+    # Aquí puedes agregar la lógica para mostrar detalles específicos del Dashboard
+  end
+
+  def stats
+    # Aquí puedes agregar la lógica para mostrar estadísticas del Dashboard
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => { registrations: 'users/registrations' }
+
+  authenticated :user do
+    root to: 'dashboard#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: 'devise/sessions#new', as: :unauthenticated_root
+  end
+  
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
